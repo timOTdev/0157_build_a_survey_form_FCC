06d6f47960a9e29275fb8e73ac95b30393f40ef4
Timothy H <timh1203@gmail.com>
2019-12-11 20:34:28 -0600

    Add 6 user stories

--- index.js
+++ index.js
@@ -25,4 +25,6 @@ INSTRUCTIONS:
 // PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
 
 // Once you have read the above messages, you can delete all comments.
-console.log("hello world")
+const footerDate = document.getElementById("currentYear")
+
+footerDate.innerText = new Date().getFullYear()
